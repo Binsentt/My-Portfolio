cd97f3829b76ebea5527a5c0a5eea411a81102d3
Binsentt <vincenttafalla2@gmail.com>
2026-06-19 08:04:00 +0800

    Update portfolio

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,16 @@ import { transformWithEsbuild } from 'vite';
 export default {
   plugins: [
     {
-    
+      name: 'transform-js-files-as-jsx',
+      async transform(code, id) {
+        if (!/src\/.*\.js$/.test(id)) {
+          return null;
+        }
+
+        return transformWithEsbuild(code, id, {
+          loader: 'jsx'
+        });
+      }
+    }
+  ]
+};
